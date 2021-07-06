8be2ae362e8d78c9aae1df1ec99efa8f49d243ce
Spicer Bak <spicer.bak.frf@gmail.com>
2021-07-06 18:05:33 -0400

    updated docstrings in the interp stuff, cleaned up

--- objMapInterp.py
+++ objMapInterp.py
@@ -7,24 +7,24 @@ import scipy
 import time
 
 def map_interp(x, y, zFluc, noise, Lx, Ly, xInt, yInt):
-    """ the main objective mapping function, converted from Bonnie Ludka's matlab script 09/2019
+    """The main objective mapping function.
+    
     Using gaussian with length scales Lx and Ly and user estimated noise_var.
     Uses fluctuation data zfluc (mean already removed by user) defined at x,y coordinates to
     interpolate to coordinates xInt, yInt. Outputs map fluctuation mapfluc and normalized mean
     square error estimate nmseEst at xInt, yInt coords
 
     Args:
-        x (array):
-        y (array):
-        zFluc (array):
-        noise (array):
-        Lx (int):
-        Ly (int):
-        xInt
-        yInt
+        x (array):  describes coordinates of zFluc
+        y (array):  describes coordinates of zFluc
+        zFluc (array): perturbation from background mean values (mean removed already)
+        noise (array):  user estimated noise
+        Lx (int): gaussian length scale in x
+        Ly (int): gaussian length scale in y
+        xInt: interpolated to these coordinates (x)
+        yInt: interpolated to these coordinates (y)
 
     Returns:
-        dgcov, dcovE, A, Aprime, mapFluc, nmseEst, dcovA, dcovA2, sigVar
 
           dictionary with following keys for all gauges
             'dgcov' (array):
@@ -36,21 +36,23 @@ def map_interp(x, y, zFluc, noise, Lx, Ly, xInt, yInt):
             'dcovA' (array):
             'dcovA2' (array):
             'sigVar' (array):
-
-    TODO:
+            
+    Notes:
+         converted from Bonnie Ludka's matlab script 09/2019
+         
+    
     """
-
-
-    begin = time.time()
-    print(begin, 'beginning mapping function')
-
+    start = time.time()
+    print('   beginning mapping function')
+    # calculate signal variance
     sigVar = np.mean(np.square(zFluc)) - np.square(noise)
+    
     # calculate data auto-covariance matrix
     X1, X2 = np.meshgrid(x, x)
     Y1, Y2 = np.meshgrid(y, y)
     dx = np.abs(X2-X1)
     dy = np.abs(Y2-Y1)
-    dcov = sigVar*np.exp(-np.square(dy/Ly)-np.square(dx/Lx))
+    dcov = sigVar * np.exp(-np.square(dy/Ly)-np.square(dx/Lx))
 
     small = np.nonzero(dcov < .000000001)
     dcov[small] = 0*np.ones((1, len(small[0])))
@@ -59,14 +61,12 @@ def map_interp(x, y, zFluc, noise, Lx, Ly, xInt, yInt):
     dcovE = dcov1 + np.identity(len(dcov))*np.square(noise)
     dcovE = spa.csr_matrix(dcovE)
 
-
-    # disp('calculate data grid covariance matrix')
+    # calculate data grid covariance matrix
     dgcov = np.zeros((len(x), len(xInt)))
     for num, j in enumerate(x):
         dx = np.abs(xInt-x[num])
         dy = np.abs(yInt-y[num])
-        dgcov[num:] = sigVar*np.exp(-np.square(dy/Ly)-np.square(dx/Lx))
-
+        dgcov[num:] = sigVar * np.exp(-np.square(dy/Ly) - np.square(dx/Lx))
 
     small2 = np.nonzero(dgcov < .0000000001)
     dgcov[small2] = 0*np.ones((1, len(small2[0])))
@@ -74,8 +74,7 @@ def map_interp(x, y, zFluc, noise, Lx, Ly, xInt, yInt):
 
     tic = time.time()
     A = scipy.linalg.lstsq(dcovE.todense(), dgcov.todense())
-    toc = time.time()
-    print(toc-tic, 'sec elapsed for matrix A')
+    print('  {:.1f} sec elapsed for matrix A'.format(time.time()-tic))
     Aprime = np.transpose(A[0])
     mapFluc = np.dot(Aprime, zFluc)
 
@@ -86,8 +85,7 @@ def map_interp(x, y, zFluc, noise, Lx, Ly, xInt, yInt):
     tic = time.time()
     for num, j in enumerate(xInt):
         nmseEst[num] = (np.transpose(A[0][:, num]).dot((dcovA2[:, num].todense() - 2 * dgcov[:, num].todense())) + sigVar)/sigVar
-    toc = time.time()
-    print(toc-tic, 'sec elapsed for error matrix')
-
+    print('  {:.1f} sec elapsed for error matrix'.format(time.time()-tic))
+    print('  {:.1f} total time for objective mapping'.format(time.time() - start))
 
     return dgcov, dcovE, A, Aprime, mapFluc, nmseEst, dcovA, dcovA2, sigVar
